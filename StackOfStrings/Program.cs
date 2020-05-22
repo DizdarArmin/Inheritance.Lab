d2fecf6c9461c1a6ed251540bece4430cf028649
DizdarArmin <armin.dizdar@gmail.com>
2020-05-22 12:57:24 +0200

    FIFO

--- StackOfStrings/Program.cs
+++ StackOfStrings/Program.cs
@@ -10,6 +10,30 @@ namespace StackOfStrings
     {
         static void Main(string[] args)
         {
+            StackOfStrings stack = new StackOfStrings();
+            
+            Console.WriteLine(stack.IsEmpty());
+
+            stack.Push("one");
+            stack.Push("two");
+            stack.Push("three");
+            stack.Push("four");
+
+
+
+            Console.WriteLine(stack.IsEmpty());
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+
+            stack.Push("five");
+            stack.Push("six");
+            stack.Push("seven");
+            stack.Push("eight");
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Peek());
+
+            Console.ReadLine();
         }
     }
 }
--- StackOfStrings/StackOfStrings.cs
+++ StackOfStrings/StackOfStrings.cs
@@ -8,7 +8,27 @@ namespace StackOfStrings
 {
     class StackOfStrings : List<string>
     {
-        List<string> data;
-       
+        private List<string> data = new List<string>();
+        public void Push(string element)
+        {
+            data.Add(element);
+
+        }
+        public bool IsEmpty()
+        {
+            return data.Count == 0;
+        }
+        public string Pop()
+        {
+            string element = data[data.Count - 1];
+            data.RemoveAt(data.Count - 1);
+            return element;
+        }
+        public string Peek()
+        {
+            return data[data.Count - 1];
+        }
+
+
     }
 }
